95b97753c83ae1444cde6ba7a84c004aaa5e8263
odya <ganzevich@gmail.com>
2024-02-12 15:21:05 +0200

    added ssl support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
+        basicSsl(),
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
